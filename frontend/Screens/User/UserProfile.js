489137d131175058eaaede131b8bb208f7666590
ximo99 <125189091+ximo99@users.noreply.github.com>
2023-05-10 16:22:09 +0200

    user profile update

--- frontend/Screens/User/UserProfile.js
+++ frontend/Screens/User/UserProfile.js
@@ -1,12 +1,24 @@
 // import dependencies
 import React, { useCallback, useContext, useEffect, useState } from "react";
-import { Text, View, ScrollView, Button, StyleSheet } from "react-native";
+import {
+  Button,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as ImagePicker from "expo-image-picker";
+import mime from "mime";
 
 // import data
 import baseURL from "../../assets/common/baseUrl";
+import colors from "../../assets/common/colors";
 
 // import context API
 import AuthGlobal from "../../Context/store/AuthGlobal";
@@ -21,29 +33,114 @@ const UserProfile = (props) => {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    // species
+    // users
     axios.get(`${baseURL}users/${userContext.user?.userId}`).then((res) => {
       setUser(res.data);
     });
+  
   }, []);
 
   return (
-    <View>
-      <Text>user profile screen: {userContext.user?.userId}</Text>
-      <Text>User name: {user?.name}</Text>
-      <Text>User email: {user?.email}</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{user?.name}</Text>
+        <Text style={styles.subtitle}>{user?.email}</Text>
+        <View style={styles.info}>
+          <Text style={styles.others}>Country: {user?.country}</Text>
+          <Text style={styles.others}>Profession: {user?.profession}</Text>
+
+          {user?.isAdmin ? (
+            <View style={styles.adminContainer}>
+              <Text style={styles.admin}>Admin user</Text>
+            </View>
+          ) : null}
+        </View>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: user?.photoUser
+              ? user.photoUser
+              : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+          }}
+        />
+        <TouchableOpacity style={styles.imagePicker}>
+          <Icon style={{ color: "white" }} name="camera" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
+    height: "100%",
+    paddingVertical: 40,
+    paddingHorizontal: 40,
+    backgroundColor: colors.background,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
   },
-  subContainer: {
-    alignItems: "center",
-    marginTop: 60,
+  title: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: colors.grey,
+    fontSize: 20,
+  },
+  info: {
+    marginVertical: 20,
+  },
+  others: {
+    color: colors.grey,
+    fontSize: 18,
+  },
+  adminContainer: {
+    backgroundColor: colors.primary,
+    alignSelf: "flex-start",
+    paddingHorizontal: 7,
+    paddingVertical: 3,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  admin: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    borderStyle: "solid",
+    borderWidth: 4,
+    padding: 0,
+    justifyContent: "center",
+    borderRadius: 100,
+    borderColor: "#e0e0e0",
+    backgroundColor: colors.background,
+    elevation: 10,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 100,
+  },
+  imagePicker: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    backgroundColor: "grey",
+    padding: 8,
+    borderRadius: 100,
+    elevation: 20,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
 
